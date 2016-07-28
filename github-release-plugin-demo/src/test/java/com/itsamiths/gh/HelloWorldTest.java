1fce4b0bc48173686aaa70469e33b83633e2523d
itsamiths <mkamithkumar@gmail.com>
2016-07-28 19:00:31 +0530

    fixed issue with class file not included in created jar

---
+++ github-release-plugin-demo/src/test/java/com/itsamiths/gh/HelloWorldTest.java
@@ -0,0 +1,9 @@
+package com.itsamiths.gh;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hi there, this is github-release-plugin demo.");
+	}
+
+}
